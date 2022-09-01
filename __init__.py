9fb81ff59753c6e9d88d7d412232a3a44b6204b1
neelimabheemishetty <112635480+neelimabheemishetty@users.noreply.github.com>
2022-09-02 01:12:11 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,80 +1,15 @@
-"""Pillow (Fork of the Python Imaging Library)
+"""distutils
 
-Pillow is the friendly PIL fork by Alex Clark and Contributors.
-    https://github.com/python-pillow/Pillow/
+The main package for the Python Module Distribution Utilities.  Normally
+used from a setup script as
 
-Pillow is forked from PIL 1.1.7.
+   from distutils.core import setup
 
-PIL is the Python Imaging Library by Fredrik Lundh and Contributors.
-Copyright (c) 1999 by Secret Labs AB.
-
-Use PIL.__version__ for this Pillow version.
-
-;-)
+   setup (...)
 """
 
-from . import _version
+import sys
 
-# VERSION was removed in Pillow 6.0.0.
-# PILLOW_VERSION was removed in Pillow 9.0.0.
-# Use __version__ instead.
-__version__ = _version.__version__
-del _version
+__version__ = sys.version[:sys.version.index(' ')]
 
-
-_plugins = [
-    "BlpImagePlugin",
-    "BmpImagePlugin",
-    "BufrStubImagePlugin",
-    "CurImagePlugin",
-    "DcxImagePlugin",
-    "DdsImagePlugin",
-    "EpsImagePlugin",
-    "FitsImagePlugin",
-    "FitsStubImagePlugin",
-    "FliImagePlugin",
-    "FpxImagePlugin",
-    "FtexImagePlugin",
-    "GbrImagePlugin",
-    "GifImagePlugin",
-    "GribStubImagePlugin",
-    "Hdf5StubImagePlugin",
-    "IcnsImagePlugin",
-    "IcoImagePlugin",
-    "ImImagePlugin",
-    "ImtImagePlugin",
-    "IptcImagePlugin",
-    "JpegImagePlugin",
-    "Jpeg2KImagePlugin",
-    "McIdasImagePlugin",
-    "MicImagePlugin",
-    "MpegImagePlugin",
-    "MpoImagePlugin",
-    "MspImagePlugin",
-    "PalmImagePlugin",
-    "PcdImagePlugin",
-    "PcxImagePlugin",
-    "PdfImagePlugin",
-    "PixarImagePlugin",
-    "PngImagePlugin",
-    "PpmImagePlugin",
-    "PsdImagePlugin",
-    "SgiImagePlugin",
-    "SpiderImagePlugin",
-    "SunImagePlugin",
-    "TgaImagePlugin",
-    "TiffImagePlugin",
-    "WebPImagePlugin",
-    "WmfImagePlugin",
-    "XbmImagePlugin",
-    "XpmImagePlugin",
-    "XVThumbImagePlugin",
-]
-
-
-class UnidentifiedImageError(OSError):
-    """
-    Raised in :py:meth:`PIL.Image.open` if an image cannot be opened and identified.
-    """
-
-    pass
+local = True
